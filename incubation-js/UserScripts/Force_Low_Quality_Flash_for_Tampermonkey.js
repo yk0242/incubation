16b01d15a1fb4bc85360316e2517f01e61e01271
yk0242 <yk0242@gmail.com>
2017-03-18 23:49:13 +0900

    (ref #50) Fix UserScript to Force Low Quality Flash to work with Tampermonkey
     * quit using "with" syntax to work with Tampermonkey
     * add code and metadata to run properly on document load end with Tampermonkey
     * add if checking to prevent internal error messages

---
+++ incubation-js/UserScripts/Force_Low_Quality_Flash_for_Tampermonkey.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name           Force Low Quality Flash for Tampermonkey
+// @version        1.0.0.20170318
+// @description    Force Low Quality Flash - adapted for Tampermonkey and adding IFRAME support -- based on http://userscripts.org/users/75739 - Force Low Quality Flash by Oscar Sodani
+// @namespace      https://github.com/yk0242
+// @author         yk0242
+// @copyright      2017
+// @license        MIT https://opensource.org/licenses/mit-license.php
+// @grant          none
+// @downloadURL    none
+// @include        *
+// @run-at         document-end
+// ==/UserScript==
+
+(function forceLowQualityFlash(){
+  
+  window.onload = function(){//wait for document (especially the contents of the IFRAMEs) to load
+    forceLowQualityFlash_act();
+    
+    //-- add support for Flash within IFRAME elements
+    // note: doesn't deal with Flash within IFRAME within another IFRAME -- add another "for" layer or recursion if need be
+    // note also that cross-origin frames canNOT be accessed this way... 
+    if(document.getElementsByTagName('iframe')){
+      for(var objs = document.getElementsByTagName('iframe'), i = objs.length-1 ; i >= 0; i--){
+        forceLowQualityFlash_act(objs[i].contentDocument);
+      }
+    }
+  };
+  
+  function forceLowQualityFlash_act(myDoc = window.document){
+  //-- based on http://userscripts.org/users/75739 - Force Low Quality Flash by Oscar Sodani
+  //-- adding error checking to prevent internal error messages
+  //-- quit using "with" syntax to work with Tampermonkey
+    var objs;
+    
+    if(document.embeds){
+      for(objs = document.embeds, i = objs.length-1; i >= 0; i--){
+        objs[i].setAttribute('quality', 'low');
+        objs[i].parentNode.appendChild(objs[i].parentNode.removeChild(objs[i]));
+      }
+    }
+    
+    if(document.getElementsByTagName('object')){
+      for(objs = document.getElementsByTagName('object'), i = objs.length-1 ; i >= 0; i--){
+        for(var c = objs[i].childNodes, j = c.length-1, set = false; j >= 0; j--) {
+          if((c[j].tagName == 'PARAM') && (c[j].getAttribute('name') == 'quality')) { c[j].setAttribute('value', 'low'); set = true; break; }
+        }
+        if(!set){
+          var myPar = objs[i].appendChild(document.createElement('param'));
+          myPar.setAttribute('name', 'quality');
+          myPar.setAttribute('value', 'low');
+        }
+        objs[i].parentNode.appendChild(objs[i].parentNode.removeChild(objs[i]));
+      }
+    }
+  }
+})();
