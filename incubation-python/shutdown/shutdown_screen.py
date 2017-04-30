c91686014e40439f94124f4003f568a59c7cdd47
yk0242 <yk0242@gmail.com>
2017-04-30 13:23:11 +0900

    (refs #51) Fixes to ShutdownScreen
     * fix code for turning screen on/off
     * fix layout, mainly so as to prevent accidentally pressing shutdown button when screen is off...

--- incubation-python/shutdown/shutdown_screen.py
+++ incubation-python/shutdown/shutdown_screen.py
@@ -5,6 +5,8 @@
 # cf http://www.ridgesolutions.ie/index.php/2013/02/22/raspberry-pi-restart-shutdown-your-pi-from-python-code/
 #    https://www.raspberrypi.org/forums/viewtopic.php?f=45&t=120296
 #    http://stackoverflow.com/questions/84882/sudo-echo-something-etc-privilegedfile-doesnt-work-is-there-an-alterna
+#    http://stackoverflow.com/questions/7389662/link-several-popen-commands-with-pipes
+#    https://raspberrypi.stackexchange.com/questions/66485/turning-rpi-official-displaytouchscreens-backlight-on-off
 
 import sys
 import subprocess
@@ -34,17 +36,20 @@ class ShutdownScreen(BoxLayout):
 
     @staticmethod
     def screen_off():
-        # command  = "echo 1 | /usr/bin/sudo tee /sys/class/backlight/rpi_backlight/bl_power"
-        command = "sudo sh -c \"echo 1 > /sys/class/backlight/rpi_backlight/bl_power\""
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
+        command1 = "echo 1"
+        command2 = "/usr/bin/sudo tee /sys/class/backlight/rpi_backlight/bl_power"
+        process1 = subprocess.Popen(command1.split(), stdout=subprocess.PIPE)
+        process2 = subprocess.Popen(command2.split(), stdin=process1.stdout, stdout=subprocess.PIPE)
+        output = process2.communicate()[0]
         print(output)
 
     @staticmethod
     def screen_on():
-        command = "echo 0 | /usr/bin/sudo tee /sys/class/backlight/rpi_backlight/bl_power"
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
+        command1 = "echo 0"
+        command2 = "/usr/bin/sudo tee /sys/class/backlight/rpi_backlight/bl_power"
+        process1 = subprocess.Popen(command1.split(), stdout=subprocess.PIPE)
+        process2 = subprocess.Popen(command2.split(), stdin=process1.stdout, stdout=subprocess.PIPE)
+        output = process2.communicate()[0]
         print(output)
 
     @staticmethod
